a0bcab3fbab40e27f081af95c156b71a81136c7f
오혜인 <108918554+hyeinhyeinhyeinhyeinhyein@users.noreply.github.com>
2022-09-27 15:39:27 +0900

    Update 과제#1.c

--- C언어_프로젝트/실습/문자열 함수/과제#1.c
+++ C언어_프로젝트/실습/문자열 함수/과제#1.c
@@ -3,10 +3,11 @@
 #include <string.h>
 
 #define TOKEN ", "
+#define FIND_CHAR '-'
 
 int main() {
 	char strary[150]; //gets함수로 입력받을 문자 배열 
-	char sstrary[150];
+
 
 	char* token; // 토큰 포인터 생성
 	char* name;
@@ -20,14 +21,44 @@ int main() {
 	token = strtok(strary, TOKEN);	// strary 문자배열이 TOKEN을 만났을 때 분리된다.
 	printf("학번 ");
 	printf("%s\n", token);	// token에 학번만 남음
-	
 
-	char *token2 = strtok(NULL, TOKEN);	// 학번 뒤의 구분자에서 잘린 곳부터 또 토큰화한다. 
+
+	char* token2 = strtok(NULL, TOKEN);	// 학번 뒤의 구분자에서 잘린 곳부터 또 토큰화한다. 
 	printf("이름 ");
 	printf("%s\n", token2); // token2에 이름만 남음
 
 	char* token3 = strtok(NULL, TOKEN);
 	printf("전화번호 ");
-	printf("%c%c%c-%c%c%c%c-%c%c%c%c", token3[0], token3[1], token3[2], token3[4], token3[5], token3[6], token3[7], token3[9], token3[10], token3[11], token3[12]);
+	//	printf("%s", token3);
+
+	// 만약 010_5650-2802일 때는?
+	// token3을 또 트큰화해야 겠지?
+
+	char* ret;
+
+		ret = strchr(token3, FIND_CHAR);
+
+		if (ret == NULL) {
+			printf("%s", token3);
+		}
+		
+		/*else {
+			char numbertmp[15];
+			numbertmp[15] = token3;
+
+			for (int i = 0; i < 3; i++)
+				printf("%c", numbertmp[i]);
+
+		*/
+
+			// printf("%d", ret-token3);
+			/*
+			char* token3_1 = strtok(token3, "-");
+			printf("%s", token3_1);
+			*/
+
+			// char* token3_1=strtok(token3_1,"-")
+		}
+
 	
 }
