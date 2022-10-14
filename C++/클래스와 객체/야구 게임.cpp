4ecfbf4890a1c4c17d545a10dbf4b0b9d20de285
오혜인 <108918554+hyeinhyeinhyeinhyeinhyein@users.noreply.github.com>
2022-10-14 18:32:56 +0900

    Update 야구 게임.cpp

--- C++/클래스와 객체/야구 게임.cpp
+++ C++/클래스와 객체/야구 게임.cpp
@@ -41,12 +41,12 @@ void Game::playgame() {
 
 	// 게임을 시작하기 앞서 난수가 서로 다른지 확인
 	if (answer2 == answer1) {	// answer2가 answer1과 같으면 새롭게 난수를 받음
-		answer2 = rand() % 10; 
+		answer2 = rand() % 10;
 		while (answer2 == answer1) // 새롭게 난수를 생성했음에도 두 값이 같으면 두 값이 달라질 때까지 난수를 받음 
 			answer2 = rand() % 10;
 	}
 
-	if (answer3 == answer1 || answer3 == answer2) { 
+	if (answer3 == answer1 || answer3 == answer2) {
 		answer3 = rand() % 10;	// answer3이 answer1이나 answer2와 같으면 새롭게 난수를 받음
 		while ((answer3 == answer1) && (answer3 == answer2))  // 그조차도 같으면 두 값이 달라질 때까지 난수를 받음
 			answer3 = rand() % 10;
@@ -55,7 +55,7 @@ void Game::playgame() {
 
 	// 사용자가 정답을 맞출 때까지 무한 반복(숫자 입력받고 스트라이크볼 알려주기를 반복)
 	while (1) {
-		
+
 		// 사용자에게 3자리 숫자를 입력 받음
 		cout << "3자리 숫자를 입력하세요: ";
 		cin >> input_answer;
@@ -65,15 +65,17 @@ void Game::playgame() {
 
 		// exit를 입력하면 프로그램 종료
 		if (input_answer[0] == 'e' && input_answer[1] == 'x' && input_answer[2] == 'i' && input_answer[3] == 't') {
-			cout << "프로그램이 종료되었습니다.";
-			break;
+			cout << "============================\n";
+			cout << "프로그램이 종료되었습니다.\n";
+			cout << "============================\n";
+			exit(1);
 		}
 
 		strike = 0; // 스트라이크 수
 		ball = 0;   // 볼 수
 
 		// 스트라이크와 볼 수를 계산
-		
+
 		for (int i = 0; i <= 2; i++) {	// 정답의 i 번째 숫자와 사용자에게 입력받은 3개의 숫자를 비교
 			for (int j = 0; j <= 2; j++) {
 				if (answer[i] == user_answer[j]) {	// 만약 정답의 i번째 숫자와 사용자에게 입력받은 3개의 숫자 중에 같은 숫자가 있을 때
@@ -88,7 +90,7 @@ void Game::playgame() {
 		score++; // 실행횟수 증가
 
 		// 숫자를 모두 맞췄을 때는 3스트라이크라는 것을 이용해 프로그램 종료 
-		if (strike == 3) { 
+		if (strike == 3) {
 			cout << "============================\n";
 			cout << score << "회 만에 정답을 맞췄습니다!\n";
 			cout << "정답: " << answer[0] << answer[1] << answer[2] << "\n";
