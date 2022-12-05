4022b6c7b6176c580559147faca856c66577d6af
오혜인 <108918554+hyeinhyeinhyeinhyeinhyein@users.noreply.github.com>
2022-12-06 01:25:43 +0900

    Update 네모로직게임.c

--- C언어_프로젝트/네모로직게임.c
+++ C언어_프로젝트/네모로직게임.c
@@ -3,6 +3,7 @@
 #define SIZE 100
 
 void print_ary(int* ary, int height, int width);
+void print_hint(int** ary, int height, int width);
 
 int main() {
 	FILE* drawfp;	//이진수로 되어있는 그림 파일 포인터 
@@ -10,7 +11,7 @@ int main() {
 	char buff[SIZE]; //fgets할 때 저장하는 버퍼 
 	int width = 0;	//그림파일의 가로길이
 	int height = 0;	//그림파일의 세로길이 
-	char ch;
+
 	drawfp = fopen("c:\\temp\\draw.txt", "r+");
 	convertfp = fopen("c:\\temp\\convert.txt", "w+");
 
@@ -39,7 +40,7 @@ int main() {
 	// 숫자를 그림화하기 > 배열에 정보 저장하기 
 	int** ary;	//이차원 배열 메모리 할당하기 
 	ary = (int**)malloc(sizeof(int*) * width);
-	for (int i= 0; i < width; i++)
+	for (int i = 0; i < width; i++)
 	{
 		ary[i] = (int*)malloc(sizeof(int) * height);
 	}
@@ -51,7 +52,7 @@ int main() {
 		for (int j = 0; j < width; j++)
 		{
 			ary[i][j] = (int)fgetc(drawfp);
-			if(ary[i][j]==10) ary[i][j] = (int)fgetc(drawfp);
+			if (ary[i][j] == 10) ary[i][j] = (int)fgetc(drawfp);
 
 		}
 	}
@@ -63,8 +64,7 @@ int main() {
 		new_ary[i] = (int*)malloc(sizeof(int) * height);
 	}
 
-	//print_ary(new_ary, height, width); // 새화면 출력 
-	print_ary(ary,height,width); // 배열 출력
+	print_hint(ary, height, width); // 배열 출력
 
 	fclose(drawfp);
 	fclose(convertfp);
@@ -75,23 +75,29 @@ int main() {
 	free(ary);
 }
 
-
-//배열 전체 출력
-void print_ary(int** ary, int height, int width)
+void print_hint(int** ary, int height, int width)
 {
-
-	int hint = 0; // 출력할 힌트 
+	// 상단 힌트 계산 변수 
+	int hint = 0;
 	int hindex = 0;
 	int windex = 0;
+	int blank_line = 0;
+	int count_blank = 0; //count_blank == width라면 그제부터는 다 0이라는 이야기
 
-	// 힌트를 저장하는 배열 메모리 할당하기
-	int ** hint_ary = (int**)malloc(sizeof(int*) * width);
+	// 왼쪽 힌트 계산 변수 
+	int left_hint = 0;
+	int left_hindex = 0;
+	int left_windex = 0;
+	int left_count_blank = 0;
+	int left_blank_line = 0;
+
+	// 상단 힌트를 저장하는 배열 메모리 할당하기
+	int** hint_ary = (int**)malloc(sizeof(int*) * width);
 	for (int i = 0; i < width; i++)
 	{
 		hint_ary[i] = (int*)malloc(sizeof(int) * height);
 	}
-
-	// 힌트 저장 배열 초기화 
+	// 상단 힌트 저장 배열 초기화 
 	for (int j = 0; j < width; j++)
 	{
 		for (int i = 0; i < height; i++)
@@ -100,18 +106,36 @@ void print_ary(int** ary, int height, int width)
 		}
 	}
 
-	// 힌트 출력하기 // 
-	for(int j=0;j<width;j++)
-	{ 
+	// 왼쪽 힌트를 저장하는 배열 메모리 할당하기 
+	int** left_hint_ary = (int**)malloc(sizeof(int*) * width);
+	for (int i = 0; i < width; i++)
+	{
+		left_hint_ary[i] = (int*)malloc(sizeof(int) * height);
+	}
+	// 왼쪽 힌트 저장 배열 초기화 
+	for (int j = 0; j < width; j++)
+	{
+		for (int i = 0; i < height; i++)
+		{
+			left_hint_ary[i][j] = NULL;
+		}
+	}
+
+	// 상단 힌트를 배열에 저장하기
+	for (int j = 0; j < width; j++)
+	{
 		for (int i = 0; i < height; i++)
 		{
 			if (ary[i][j] == '1') {
 				hint++;
 			}
 			else {
-				hint_ary[hindex][windex] = hint;
-				hint = 0;
-				hindex++;
+				if (hint != 0)
+				{
+					hint_ary[hindex][windex] = hint;
+					hint = 0;
+					hindex++;
+				}
 			}
 		}
 		hint_ary[hindex][windex] = hint;
@@ -119,40 +143,117 @@ void print_ary(int** ary, int height, int width)
 		windex++;
 		hindex = 0;
 	}
-
+	// 상단 힌트 줄 커트 	
 	for (int i = 0; i < height; i++)
 	{
+		for (int j = 0; j < width; j++)
+		{
+			if (hint_ary[i][j] == NULL)
+				count_blank++;
+		}
+		if (count_blank == width)
+		{
+			blank_line = i;
+			break;
+		}
+		count_blank = 0;
+	}
+
+
+	// 왼쪽힌트를 배열에 저장하기
+	for (int j = 0; j < height; j++)
+	{
+		for (int i = 0; i < width; i++)
+		{
+			if (ary[j][i] == '1') {
+				left_hint++;
+			}
+			else {
+				if (left_hint != 0)
+				{
+					left_hint_ary[left_hindex][left_windex] = left_hint;
+					left_hint = 0;
+					left_windex++;
+				}
+			}
+		}
+		left_hint_ary[left_hindex][left_windex] = left_hint;
+		left_hint = 0;
+		left_hindex++;
+		left_windex = 0;
+	}
+
+
+	// 왼쪽 힌트 줄 커트 
+	for (int j = 0; j < height; j++)
+	{
+		for (int i = 0; i < width; i++)
+		{
+			if (left_hint_ary[i][j] == NULL)
+				left_count_blank++;
+		}
+
+		if (left_count_blank == height)
+		{
+			left_blank_line = j;
+			break;
+		}
+
+		left_count_blank = 0;
+	}
+	printf("왼쪽 줄 커트 %d\n", left_blank_line);
+
+
+
+
+	// 상단 힌트 출력
+	for (int i = 0; i < blank_line; i++)
+	{
+		for (int t = 0; t < left_blank_line; t++)
+			printf("  ");
+
 		for (int j = 0; j < width; j++)
 		{
 			if (hint_ary[i][j] != NULL)	printf("%d|", hint_ary[i][j]);
-			else printf("0|");
+			else {
+				int blank_line = 0;
+
+				printf(" |");
+			}
+		}
+		printf("\n");
+	}
+
+
+
+	int for_height = 0;
+	// 왼쪽 힌트 출력
+
+	for (int i = 0; i < width; i++)
+	{
+		// 숫자 힌트 출력
+		for (int j = 0; j < left_blank_line; j++)
+		{
+			if (left_hint_ary[i][j] != NULL) 
+			{
+				printf("%d|", left_hint_ary[i][j]); 
+			}
+
+			else 
+			{
+				printf(" |");
+			}
+		}
 		
-		}
-		printf("\n");
-	}
 
-
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
+		// 그림판 출력 
+		for (int b = 0; b < width; b++)
 		{
-			if (ary[i][j] == '1') printf("■");
+			if (ary[for_height][b] == '1') printf("■");
 			else printf("□");
-			//printf("%c", ary[i][j]);
 		}
 		printf("\n");
+		for_height++;
 	}
-
-	/*
-	// 숫자로 출력하기 
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			printf("%c", ary[i][j]);
-
-		}
-		printf("\n");
-	}
-	*/
+	
 }
